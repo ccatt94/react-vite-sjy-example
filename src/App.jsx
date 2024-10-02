7ce8716a6f6b49858349a8ae130b110e75d2e244
WIN-ADK7AOT0SOJ\ICT02_20 <sally940828@naver.com>
2024-10-02 11:40:01 +0900

    부트스트랩 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BootStrapButton from './BootstrapExample'
+import Header from './components/Header'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
 
 function App() {
-  const hello = "hi";
+    const hello = 'hi'
 
-  const style = {
-    fontSize : 100,
-    color : 'red'
-  }
+    const style = {
+        fontSize: 100,
+        color: 'red',
+    }
 
-  return (
-    <>
-      <div style = {style}>{hello}</div>
-      <div>{hello}</div>
-    </>
-  )
+    return (
+        <>
+            <Header></Header>
+            <div style={style}>{hello}</div>
+            <div>{hello}</div>
+            <BootStrapButton />
+        </>
+    )
 }
 
 export default App
