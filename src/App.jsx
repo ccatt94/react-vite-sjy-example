c091da232c0e7c0c10268702dd25c95374a9cc59
WIN-ADK7AOT0SOJ\ICT02_20 <sally940828@naver.com>
2024-10-02 12:17:20 +0900

    라우터 이해

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,24 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import BootStrapButton from './BootstrapExample'
 import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BoardPage from './pages/BoardPage'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
 
 function App() {
-    const hello = 'hi'
-
-    const style = {
-        fontSize: 100,
-        color: 'red',
-    }
-
-    return (
-        <>
-            <Header></Header>
-            <div style={style}>{hello}</div>
-            <div>{hello}</div>
-            <BootStrapButton />
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/board" element={<BoardPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
