f1a5fe2658260bddbbefc449d0cede4bd8a19ff3
WIN-ADK7AOT0SOJ\ICT02_20 <sally940828@naver.com>
2024-10-04 11:55:10 +0900

    조건부연산자설명

--- src/components/Hello3.jsx
+++ src/components/Hello3.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const Hello3 = ({ name, color, isSpecial, isName }) => {
   return (
     //prettier-ignore
+    //자바스크립트는 if문 사용 불가 => 그래서 3항연산자 사용함
+    // for문 또한 쓰지 못하기 때문에 map / filter 함수 사용
     <div style={{color : color}}>
       하이 {isName ? name : null} <br/>
       색상 : {color} <br/>
