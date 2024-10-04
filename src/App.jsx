655c10d9848567bb5ceffb5fd912136822928d76
WIN-ADK7AOT0SOJ\ICT02_20 <sally940828@naver.com>
2024-10-04 09:35:17 +0900

    라우팅 응용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import BoardPage from './pages/BoardPage'
 import ProfilePage from './pages/ProfilePage'
+import routes from './route/Routes'
+import HeaderNav from './components/HeaderNav'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
@@ -14,10 +16,20 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <HeaderNav />
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
+          {/* <Route path="/" element={<HomePage />}></Route>
           <Route path="/board" element={<BoardPage />}></Route>
-          <Route path="/profile" element={<ProfilePage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route> */}
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={route.path || index}
+                path={route.path}
+                element={route.element}
+              ></Route>
+            )
+          })}
         </Routes>
       </BrowserRouter>
     </>
